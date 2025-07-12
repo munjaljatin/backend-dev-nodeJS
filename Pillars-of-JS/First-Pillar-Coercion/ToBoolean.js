876eb3a4d345574b5e70e41921f3cca4292a38a3
Jatin Munjal <munjal.jatin24@gmail.com>
2025-07-13 05:20:48 +0530

    Coercion revision

---
+++ Pillars-of-JS/First-Pillar-Coercion/ToBoolean.js
@@ -0,0 +1,8 @@
+// ToBoolean abstract operation => To mimic use logicl Not (!) operator and relational opeartors
+
+console.log(!NaN);
+console.log(!+0);
+console.log(!-0);
+console.log(!"");
+console.log(!undefined);
+console.log(!30);
--- Pillars-of-JS/First-Pillar-Coercion/equality.js
+++ Pillars-of-JS/First-Pillar-Coercion/equality.js
@@ -1,3 +1,12 @@
 // abstract equality operator
 console.log(null == undefined);
 console.log(100 == "abc");
+
+let x = 10;
+let name = "Jatin";
+console.log(x === name);
+
+console.log(undefined == null);
+
+let y = "string";
+console.log(x == y);
