31ce7e7045bb9274b1f28ace276b2a3dbb59d857
Jatin Munjal <munjal.jatin24@gmail.com>
2025-07-12 22:10:59 +0530

    Coercion revision

--- Pillars-of-JS/First-Pillar-Coercion/ToPrimitive.js
+++ Pillars-of-JS/First-Pillar-Coercion/ToPrimitive.js
@@ -28,3 +28,34 @@ console.log(obj.toString());
 
 console.log(obj.valueOf());
 console.log(typeof obj.valueOf());
+
+const obj1 = {
+  name: "Jatin",
+  valueOf() {
+    return "11";
+  },
+  toString() {
+    return "11";
+  },
+};
+
+console.log(100 - obj1);
+console.log(obj1);
+console.log(obj1.toString());
+console.log(obj1.valueOf());
+
+let obj5 = {
+  x: 10,
+  y: 10,
+  toString() {
+    return {};
+  },
+};
+
+let num = 10;
+// This will throw and TypeError Exception
+// console.log(num - obj5);
+
+// Addition Operator -> The Addition operatory either performs the string concatenation or numeric addition
+console.log("20" + {});
+console.log(20 + {});
