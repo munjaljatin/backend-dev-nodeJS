424d451b6b5f1f10914a1a1fda0fe38a692c08cb
Jatin Munjal <munjal.jatin24@gmail.com>
2025-07-20 17:05:47 +0530

    Scopes updated

--- Pillars-of-JS/Second-Pillar-Scopes/scopes.js
+++ Pillars-of-JS/Second-Pillar-Scopes/scopes.js
@@ -1,12 +1,40 @@
-const x = "x decalred outside the function";
+console.log(firstName);
+// console.log(lastName);
+const x = "x declared outside the function";
+var firstName = "Jatin";
+let lastName = "Munjal";
 
-function exampleFunction()
-{
-    console.log("Inside the function");
-    console.log(x);
+function exampleFunction() {
+  console.log("Inside the function");
+  console.log(x);
 }
 
 exampleFunction();
 
 console.log("Outside the function");
 console.log(x);
+
+// Block Scope
+{
+  const x = 10;
+  let y = 20;
+  console.log(x);
+  console.log(y);
+}
+
+// Another Block Scope
+{
+  //   var y = 10;
+}
+
+// console.log(y);
+
+const funExp = function () {
+  var y = 10;
+};
+
+{
+  var y = 20;
+}
+
+console.log(y);
