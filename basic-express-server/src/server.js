78431dcaf0008f29aeeb4f020e556fb8f1de90f3
Jatin Munjal <munjal.jatin24@gmail.com>
2026-04-09 22:45:10 +0530

    initialize basic express server with middleware and routes

---
+++ basic-express-server/src/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+// when we call the express function then a new express server onject is created
+const app = express();
+const PORT = 3000;
+
+// defining the express middlewares
+// middleware \1
+const middleware1 = (req, res, next) => {
+  console.log("Inside the MiddleWare 1");
+  next();
+};
+
+// middleware 2
+const middleware2 = (req, res, next) => {
+  console.log("Inside the MiddleWare 2");
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.send("Hi server is running");
+});
+
+app.get("/api", middleware1, middleware2, (req, res) => {
+  // everytime, when the user calls the /api then this callback will be called
+  console.log("You are /api route");
+  return res.json({ message: "Success" }); // here the object we are passing nothing but a JS object itself
+});
+app.listen(PORT, () => {
+  console.log(`Server is Running on PORT ${PORT}`);
+});
